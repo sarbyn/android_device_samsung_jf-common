d0d89846b598498aa2aa283c6ab16b2a9febec55
John <sombree@gmail.com>
2015-07-15 23:33:21 +0200

    Fix bluetooth

--- jf-common.mk
+++ jf-common.mk
@@ -180,7 +180,8 @@ PRODUCT_PACKAGES += \
     init.qcom.rc \
     init.qcom.usb.rc \
     init.target.rc \
-    ueventd.qcom.rc
+    ueventd.qcom.rc \
+	init.qcom.bt.sh
 
 # Thermal
 PRODUCT_COPY_FILES += \
